08d99f1b74f0dbcb595369d101369f5bc227c9bf
Nichidanil <nichi.par@mail.ru>
2019-05-04 15:10:35 +0700

    Add constructor(1) for matrix

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,4 +1,14 @@
-#include <iostream>
 #include "matrix.h"
+#include <iostream>
 
 using namespace std;
+
+matrix::matrix()
+{
+    rows = 1;
+    cols = 1;
+    mat = new float*[rows];
+    for (int i = 0; i < rows; i++) {
+        mat[i] = new float[cols];
+    }
+}
