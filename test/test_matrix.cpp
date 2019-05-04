e2bec7fb274f63c6379455b3620af6a844ed0e15
Devyatiyarov <devyatiyarov00@bk.ru>
2019-05-04 23:18:59 +0700

    Add test constructor(2) for matrix

--- test/test_matrix.cpp
+++ test/test_matrix.cpp
@@ -25,3 +25,23 @@ TEST_CASE("matrix is created in constructor№1", "[matrix]")
     REQUIRE(matr.rows == 1);
     REQUIRE(matr.cols == 1);
 }
+
+TEST_CASE("matrix is created in constructor№2", "[matrix]")
+{
+    int rows = 784;
+    int cols = 10;
+    matrix matr1(rows, cols);
+
+    REQUIRE(matr1.mat != NULL);
+    REQUIRE(matr1.rows == rows);
+    REQUIRE(matr1.cols == cols);
+
+    rows = 10;
+    cols = 784;
+    matrix matr2(rows, cols, 1);
+
+    REQUIRE(matr2.mat != NULL);
+    REQUIRE(matr2.rows == rows);
+    REQUIRE(matr2.cols == cols);
+    REQUIRE(matr2.mat[2][235] != 0);
+}
