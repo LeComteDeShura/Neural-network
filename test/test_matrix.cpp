10748b9a2a7492dd8c826e7ccc0be401eafb8d3d
Devyatiyarov <devyatiyarov00@bk.ru>
2019-05-04 22:58:43 +0700

    Add test function randM

--- test/test_matrix.cpp
+++ test/test_matrix.cpp
@@ -4,6 +4,19 @@
 #include "matrix.h"
 #include <iostream>
 
+TEST_CASE("Random number from M - 0.5 to N - 0.5", "[randM]")
+{
+    double M = 0.1;
+    double N = 0.99;
+    REQUIRE(randM(M, N) > (0.1 - 0.5));
+    REQUIRE(randM(M, N) < (0.99 - 0.5));
+
+    M = 10.0;
+    N = 12.0;
+    REQUIRE(randM(M, N) > (10 - 0.5));
+    REQUIRE(randM(M, N) < (12 - 0.5));
+}
+
 TEST_CASE("matrix is created in constructor№1", "[matrix]")
 {
     matrix matr;
