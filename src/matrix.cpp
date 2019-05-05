a54d12100570b4854058d82843d33a26fab9c079
Nichidanil <nichi.par@mail.ru>
2019-05-05 14:58:32 +0700

    Add initial method for matrix

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -42,3 +42,31 @@ matrix::matrix(int r, int c, int total ){
                 mat[i][j] = randM(0.1, 0.99);
     }
 }
+
+void matrix::init(int r, int c, int total ){
+    for (int i = 0; i < rows; i++) {
+        delete []mat[i];
+    }
+    delete []mat;
+    rows = r;
+    cols = c;
+    mat = new float *[rows];
+    for (int i = 0; i < rows; i++) {
+        mat[i] = new float [cols];
+    }
+
+    if (total == 0) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                mat[i][j] = 0;
+            }
+        }
+    }
+
+
+    if(total == 1){
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                mat[i][j] = randM(0.1, 0.99);
+    }
+}
