bb2a8d7afca2da8373f2cc3dc3fcb4385beb63b4
LeComteDeShura <chernicovegor@gmail.com>
2019-05-20 05:25:40 +0200

    Add sigmoida func

--- src/NeuralNetworks.cpp
+++ src/NeuralNetworks.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include "matrix.h"
+#include "NeuralNetworks.h"
+#include <cmath>
+
+matrix sigmoida(matrix Matrix){
+    float e = 2.7180;
+    matrix Matrixp(Matrix.rows, Matrix.cols);
+
+    for (int i = 0; i < Matrix.rows; i++) {
+        for (int j = 0; j < Matrix.cols; j++) {
+             Matrixp.mat[i][j] = 1 / (1 + pow(e, (Matrix.mat[i][j] * -1)));
+        }
+    }
+    return Matrixp;
+}
