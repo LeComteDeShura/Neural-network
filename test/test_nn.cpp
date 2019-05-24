475f264a7dfe7e0f14842db1a4d16ddcb9fc1f49
Devyatiyarov <devyatiyarov00@bk.ru>
2019-05-24 23:58:29 +0700

    Add test function sigmoida

--- test/test_nn.cpp
+++ test/test_nn.cpp
@@ -1,6 +1,39 @@
 #define CATCH_CONFIG_MAIN
 
+#include "matrix.h"
+#include "NeuralNetworks.h"
 #include "catch.hpp"
-#include "NeuralNetwork.h"
 #include <iostream>
 
+TEST_CASE("calc_test", "[sigmoida]")
+{
+    matrix A(2,2);
+    A.mat[0][0] = 0.1;
+    A.mat[1][0] = 0.2;
+    A.mat[1][1] = 0.5;
+    A.mat[0][1] = 0.99;
+
+    matrix B;
+    B = sigmoida(A);
+    B.mat[0][0] = int(B.mat[0][0] * 10000)/10000.0;
+    B.mat[1][0] = int(B.mat[1][0] * 10000)/10000.0;
+    B.mat[1][1] = int(B.mat[1][1] * 10000)/10000.0;
+    B.mat[0][1] = int(B.mat[0][1] * 10000)/10000.0;
+
+    matrix C(2,2);
+    C.mat[0][0] = 0.5249;
+    C.mat[1][0] = 0.5498;
+    C.mat[1][1] = 0.6224;
+    C.mat[0][1] = 0.729;
+
+    int count = 0;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            if(C.mat[i][j] == B.mat[i][j]){
+                count++;
+            }
+        }
+    }
+    std::cout << B.mat[1][0] << '\n' << C.mat[1][0];
+    REQUIRE(count == 4);
+}
