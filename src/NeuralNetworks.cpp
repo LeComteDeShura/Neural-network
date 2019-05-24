107a05b721161b1d1b39cac1702ebd22a4e9887a
LeComteDeShura <chernicovegor@gmail.com>
2019-05-24 16:47:33 +0700

    Add weight Initialization function

--- src/NeuralNetworks.cpp
+++ src/NeuralNetworks.cpp
@@ -27,3 +27,43 @@ NeuralNetwork::NeuralNetwork( int input_nodes, int hidden_nodes, int output_node
     wih.init(hn, in, 1);
     who.init(on, hn, 1);
 }
+
+void NeuralNetwork::init_w()
+{
+    FILE* file;
+    char* mass;
+    char* pch;
+    char buff[4500000];
+
+    file = fopen("csvfiles//w.csv", "r");
+
+    mass = fgets(buff, 4000000, file);
+
+    for (int i = 0; i < hn; i++) {
+        for (int j = 0; j < in; j++) {
+            if (i == 0 && j == 0) {
+                pch = strtok(mass, ",");
+                wih.mat[0][0] = atof(pch);
+                continue;
+            }
+            pch = strtok(NULL, ",");
+
+            wih.mat[i][j] = atof(pch);
+        }
+    }
+
+    mass = fgets(buff, 50000, file);
+    for (int i = 0; i < on; i++) {
+        for (int j = 0; j < hn; j++) {
+            if (i == 0 && j == 0) {
+                pch = strtok(mass, ",");
+                who.mat[0][0] = atof(pch);
+                continue;
+            }
+            pch = strtok(NULL, ",");
+            who.mat[i][j] = atof(pch);
+        }
+    }
+
+    fclose(file);
+}
