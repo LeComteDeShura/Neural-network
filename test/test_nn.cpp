f13713af22c8c8fb42dc4ae0b1b6a07bb513316c
Devyatiyarov <devyatiyarov00@bk.ru>
2019-05-25 22:01:34 +0700

    Add test function train

--- test/test_nn.cpp
+++ test/test_nn.cpp
@@ -77,3 +77,64 @@ TEST_CASE("calc_test_recognition", "[recognition]")
     REQUIRE(exp.mat[0][0] == input.mat[1][1]);
     REQUIRE(exp.mat[0][0] == input.mat[0][1]);
 }
+
+TEST_CASE("calc_test_train", "[train]")
+{
+    NeuralNetwork nn(2, 2, 2, 0.333, 2, 2);
+
+    nn.wih.mat[0][0] = 2;
+    nn.wih.mat[1][0] = 2;
+    nn.wih.mat[0][1] = 2;
+    nn.wih.mat[1][1] = 2;
+
+    nn.who.mat[1][0] = 2;
+    nn.who.mat[0][0] = 2;
+    nn.who.mat[0][1] = 2;
+    nn.who.mat[1][1] = 2;
+
+    matrix input(2, 2);
+    input.mat[1][0] = 2;
+    input.mat[0][0] = 2;
+    input.mat[0][1] = 2;
+    input.mat[1][1] = 2;
+
+    matrix targets(2, 2);
+    targets.mat[1][0] = 0.1;
+    targets.mat[0][0] = 0.1;
+    targets.mat[0][1] = 0.1;
+    targets.mat[1][1] = 0.9;
+
+
+    matrix exp_wih(2, 2);
+    exp_wih.mat[1][0] = 1.99908;
+    exp_wih.mat[0][0] = 1.99908;
+    exp_wih.mat[0][1] = 1.99908;
+    exp_wih.mat[1][1] = 1.99908;
+    matrix exp_who(2, 2);
+    exp_who.mat[1][0] = 1.99573;
+    exp_who.mat[0][0] = 1.9922;
+    exp_who.mat[0][1] = 1.9922;
+    exp_who.mat[1][1] = 1.99573;
+
+    nn.train(input, targets);
+
+    nn.wih.mat[0][0] = int(nn.wih.mat[0][0] * 100000)/100000.0;
+    nn.wih.mat[1][0] = int(nn.wih.mat[1][0] * 100000)/100000.0;
+    nn.wih.mat[1][1] = int(nn.wih.mat[1][1] * 100000)/100000.0;
+    nn.wih.mat[0][1] = int(nn.wih.mat[0][1] * 100000)/100000.0;
+
+    nn.who.mat[0][0] = int(nn.who.mat[0][0] * 100000)/100000.0;
+    nn.who.mat[1][0] = int(nn.who.mat[1][0] * 100000)/100000.0;
+    nn.who.mat[1][1] = int(nn.who.mat[1][1] * 100000)/100000.0;
+    nn.who.mat[0][1] = int(nn.who.mat[0][1] * 100000)/100000.0;
+
+    REQUIRE(exp_wih.mat[0][0] == nn.wih.mat[0][0]);
+    REQUIRE(exp_wih.mat[1][0] == nn.wih.mat[1][0]);
+    REQUIRE(exp_wih.mat[0][1] == nn.wih.mat[0][1]);
+    REQUIRE(exp_wih.mat[1][1] == nn.wih.mat[1][1]);
+
+    REQUIRE(exp_who.mat[0][0] == nn.who.mat[0][0]);
+    REQUIRE(exp_who.mat[1][0] == nn.who.mat[1][0]);
+    REQUIRE(exp_who.mat[0][1] == nn.who.mat[0][1]);
+    REQUIRE(exp_who.mat[1][1] == nn.who.mat[1][1]);
+}
