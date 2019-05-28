b81706e3bb795553a8bcb7fc8f55011dfbadd844
LeComteDeShura <chernicovegor@gmail.com>
2019-05-28 09:18:40 +0700

    Add launch efficiency test function

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 
 void launchTrain(NeuralNetwork ner);
+void launchRecognition(NeuralNetwork ner);
 
 int main() {
 
@@ -43,4 +44,56 @@ void launchTrain(NeuralNetwork ner)
         }
         fclose(file);
     }
+}
+
+void launchTestEfficiency(NeuralNetwork ner)
+{
+    FILE* file;
+    char* mass;
+    char* pch;
+    char buff[5000];
+    file = fopen("mnist_test.csv", "r");
+
+    matrix inputs(784, 1);
+    matrix final_outputs;
+
+    float max;
+    int label;
+    int correct_label;
+    int count_label = 0;
+
+    ner.init_w();
+
+    int count = 0;
+    while (count < 10001) {
+        mass = fgets(buff, 5000, file);
+        pch = strtok(mass, ",");
+        correct_label = atoi(pch);
+
+        for (int i = 0; i < ner.in; i++) {
+            pch = strtok(NULL, ",");
+            inputs.mat[i][0] = (atoi(pch) / 255.001 * 0.99) + 0.01;
+        }
+        final_outputs = ner.recognition(inputs);
+
+        max = final_outputs.mat[0][0];
+        label = 0;
+        for (int i = 0; i < ner.on; i++) {
+            if (max < final_outputs.mat[i][0]) {
+                label = i;
+                max = final_outputs.mat[i][0];
+            }
+        }
+
+        if (label == correct_label) {
+            count_label++;
+        }
+
+        count++;
+    }
+
+    fclose(file);
+    std::cout << '\n'
+              << "эффективность - " << (count_label / (count + 0.1)) * 100.1
+              << '%' << '\n';
 }
